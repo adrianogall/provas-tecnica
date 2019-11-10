5d2eee5c3f36c624bdb8dc9d6580bd5e9c586e37
Adriano Gall <adriano@GAD.local>
2019-11-10 19:51:33 -0300

    Commit de estrutura inicial.

---
+++ front-end/categoria-owme/gulpfile.js
@@ -0,0 +1,29 @@
+const { src, dest, parallel } = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
+
+function html() {
+  return src('client/templates/*.pug')
+    .pipe(pug())
+    .pipe(dest('build/html'))
+}
+
+function css() {
+  return src('project/css/style.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(dest('project/css/build/'))
+}
+
+function js() {
+  return src('project/js/script.js', { sourcemaps: true })
+    .pipe(concat('script.min.js'))
+    .pipe(dest('project/js/build/', { sourcemaps: true }))
+}
+
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.default = parallel(html, css, js);
