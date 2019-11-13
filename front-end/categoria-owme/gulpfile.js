42481db7856a59fb13ce8d2afe02521ed2387591
Adriano Gall <adriano@GAD.local>
2019-11-13 00:02:43 -0300

    Ajustes do gulp e build.

--- front-end/categoria-owme/gulpfile.js
+++ front-end/categoria-owme/gulpfile.js
@@ -1,28 +1,61 @@
-const { src, dest, parallel } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
+const del = require('del');
+const babel = require('gulp-babel');
+const minify = require('gulp-minify');
+const autoprefixer = require('gulp-autoprefixer');
+
+function clean() {
+  return del(['build']);
+}
+
+function fonts() {
+  return gulp.src('project/fonts/*.*')
+    .pipe(gulp.dest('build/fonts/'))
+}
+
+function json() {
+  return gulp.src('project/files/*.json')
+    .pipe(gulp.dest('build/files/'))
+}
 
 function html() {
-  return src('client/templates/*.pug')
-    .pipe(pug())
-    .pipe(dest('build/html'))
+  return gulp.src('index.html')
+    .pipe(gulp.dest('build/'))
 }
 
 function css() {
-  return src('project/css/style.scss')
+  return gulp.src('project/css/style.scss')
     .pipe(sass())
+    .pipe(autoprefixer({
+        cascade: false
+    }))
     .pipe(minifyCSS())
-    .pipe(dest('project/css/build/'))
+    .pipe(gulp.dest('build/css/'))
 }
 
 function js() {
-  return src('project/js/script.js', { sourcemaps: true })
+  return gulp.src('project/js/script.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(minify())
     .pipe(concat('script.min.js'))
-    .pipe(dest('project/js/build/', { sourcemaps: true }))
+    .pipe(gulp.dest('build/js/'))
 }
 
+function watch(){
+ gulp.watch('project/js/script.js', js)
+}
+
+exports.clean = clean;
+exports.fonts = fonts;
 exports.js = js;
 exports.css = css;
 exports.html = html;
-exports.default = parallel(html, css, js);
+exports.json = json;
+exports.default = watch;
+exports.build = gulp.series(clean, gulp.parallel(fonts, html, css, js, json));
+exports.dev = gulp.series(clean, gulp.parallel(fonts, html, css, js, json, watch));
--- front-end/categoria-owme/project/js/script.js
+++ front-end/categoria-owme/project/js/script.js
@@ -6,18 +6,18 @@
 
 //Busca os parâmetros da url para executar alistagem de produtos
 const getParameterUrl = () => {
-    
+
     let query = location.search.slice(1);
     let partes = query.split('&');
     let data = {};
 
     partes.forEach(parte => {
         if(parte != ''){
-            let chaveValor = parte.split('='); 
+            let chaveValor = parte.split('=');
             let chave = chaveValor[0];
             let valor = chaveValor[1];
             data[chave] = valor;
-        }            
+        }
     });
     return data;
 }
@@ -26,30 +26,30 @@ const getParameterUrl = () => {
 let numPage = 1;
 const  qntPage = 20;
 let Xmlhttp = new XMLHttpRequest();
-let arqJsonProd = "../../project/files/mock-products.json";
+let arqJsonProd = "files/mock-products.json";
 let getParamUrl = getParameterUrl();
 let filter = "valor-desc";
 let urlInitial = true;
-    
+
 //Verifica de tem parâmetros de filtros passados na url no carregamento da página
-if(getParamUrl.hasOwnProperty('q')){  
+if(getParamUrl.hasOwnProperty('q')){
 
     urlInitial = false;
     filter = getParamUrl.q;
     window.document.getElementById("change-filter").value = filter;
-    
+
     if(getParamUrl.hasOwnProperty('page'))
         numPage = getParamUrl.page;
-} 
+}
 
 //Função start para as funcinalidades de filtro e paginação
 const getStartListProd = (filter, numPage) => {
-    
+
     document.getElementById("bg-loader").style.display = "block";
     objJsonProd = localStorage.getItem('objJsonProd');
     objJsonProd = JSON.parse(objJsonProd).products;
     orderFilterJson(objJsonProd, filter, numPage);
-    setUrlFilter(filter, numPage);    
+    setUrlFilter(filter, numPage);
     document.getElementById("bg-loader").style.display = "none";
 }
 
@@ -77,24 +77,24 @@ const request = obj => {
 request({url: arqJsonProd, method: 'GET'})
     .then(data => {
         localStorage.setItem("objJsonProd", data);
-        getStartListProd(filter, numPage);  
+        getStartListProd(filter, numPage);
     })
     .catch(error => {
         console.log(error);
 });
- 
+
 //Atualiza os parâmetros na url
-const setUrlFilter = (filter, numPage) => 
+const setUrlFilter = (filter, numPage) =>
     window.history.replaceState('', '', window.location.href.split('?')[0] + "?q=" + filter + "&page=" + numPage);
 
 //Contrutor da listagem de produtos
 const constListProducts = (arrProd, numPage) => {
 
-    var htmlListProd = "",
-        targetProd = parseInt(numPage-1) * parseInt(qntPage),
-        i2 = 0;
+    let htmlListProd = "";
+    let targetProd = parseInt(numPage-1) * parseInt(qntPage);
+    let i2 = 0;
 
-    for(var i = 0; i < arrProd.length; i++) {
+    for(let i = 0; i < arrProd.length; i++) {
         if(targetProd <= i && i2 < qntPage){
             htmlListProd += `<li>
                                 <div class="cont">
@@ -107,7 +107,7 @@ const constListProducts = (arrProd, numPage) => {
                                 </div>
                             </li>`;
             i2++;
-        }                       
+        }
     }
     document.getElementById("list-products").innerHTML = htmlListProd;
 }
@@ -115,40 +115,40 @@ const constListProducts = (arrProd, numPage) => {
 //Contrutor da paginação
 const constPaginator = (objList, filter, numPage) => {
 
-    let qntTotal = Object.keys(objList).length,
-        htmlListPag = "",
-        contrPag = parseInt(qntTotal-1) / parseInt(qntPage);
-        
+    let qntTotal = Object.keys(objList).length;
+    let htmlListPag = "";
+    let contrPag = parseInt(qntTotal-1) / parseInt(qntPage);
+
     for(let i = 1; i <= Math.ceil(contrPag); i++) {
         if(numPage == i)
             htmlListPag += `<li class="active"><span>` + i + `</span></li>`;
         else
-            htmlListPag += `<li><a href="javascript:void(0)" onClick="getStartListProd('` + filter + `',` + i + `);">` + i + `</a></li>`;             
+            htmlListPag += `<li><a href="javascript:void(0)" onClick="getStartListProd('` + filter + `',` + i + `);">` + i + `</a></li>`;
     }
     document.getElementById("pag-prod-nav").innerHTML = htmlListPag;
 }
 
 //Ordena o abjeto de produtos de acordo com o filtro
-const orderFilterJson = (objList, filter, numPage) => { 
+const orderFilterJson = (objList, filter, numPage) => {
 
     let arrFilter = filter.split('-');
-    objList.sort(dynamicSort(arrFilter));   
+    objList.sort(dynamicSort(arrFilter));
     let arrProd = Object.values(objList);
-    
+
     //Construtor da listagem de produtos
     constListProducts(arrProd, numPage);
-    
+
     //Construtor da paginação
     constPaginator(objList, filter, numPage);
-    
+
     window.scrollTo(0, 50);
 }
 
 //Ordena o object de produtos conforme os parâmetros passados
 const dynamicSort = arrtFilter => {
 
-    let sortOrder = 1,
-        result;
+    let sortOrder = 1;
+    let result;
 
     if(arrtFilter[0] === "-") {
         sortOrder = -1;
